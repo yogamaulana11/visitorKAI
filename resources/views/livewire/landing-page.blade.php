f685abd6b258b63494e933c8ff2f1b0d80f6435e
ekovegeance <me@ekovegeance.com>
2023-10-25 19:17:52 +0700

    update navbar

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -1,4 +1,40 @@
 <div>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-    <h1>Hello World 1213</h1>
+    <nav class="navbar navbar-expand-lg" style="background-color: #EAEAEA">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <img src="{{ asset('assets/img/logo.png') }}" alt="KAI"  height="24">
+              </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            {{-- button login --}}
+            <form class="d-flex">
+              <button class="btn text-light me-5" style="background-color: #ED6B23" type="submit">Login</button>
+          </div>
+        </div>
+      </nav>
 </div>
