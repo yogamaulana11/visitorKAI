69a68eff629e34744b192162c45927365244def1
yogamaulana11 <yogamaulanaa@gmail.com>
2023-11-02 20:50:57 +0700

    update lupa password2

--- resources/views/livewire/admin/lupapassword2.blade.php
+++ resources/views/livewire/admin/lupapassword2.blade.php
@@ -1,3 +1,42 @@
 <div>
-   
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand ms-4" href="/">
+                <img src="{{ asset('assets/img/logo.png') }}" alt="KAI"  height="24">
+            </a>
+            <div class="me-4">
+                <a href="/login" class="btn text-light rounded-pill fw-semibold" style="background-color: #ED6B23; border-color: #ED6B23">Masuk
+                </a>
+                <a href="/register" class="btn text-light rounded-pill fw-semibold" style="background-color: #2D2A70; border-color: #2D2A70">Registrasi
+                </a>
+                </div>
+        </div>
+      </nav>
+          <div class="container py-5">
+          <div class="container mt-5">
+              <div class="mx-auto" style="width: 500px; height: 500px;">
+              <div class="container mx-3 border border-2 rounded-3 shadow-lg">
+              <form>
+                      <div class="mb-2">
+                        <h4 class="text-center mt-3">Lupa Kata Sandi</h4>
+                          <label for="exampleInputPassword1" class="form-label">Email</label>
+                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan email Anda">
+                      </div>
+                      <div 
+                        class="mt-3 form-check">
+                      </div>
+                        <button type="submit" class="btn text-light rounded-pill  " style="background-color: #ED6B23">Reset Kata Sandi</button>
+              </form>
+              <p class="text-center">
+                <i class="bi bi-arrow-left-square"></i>
+                <a href ="" class="text-decoration-none text-dark">Kembali Untuk Masuk</a>
+                </p>
+              </div>
+          </div>
+  <footer>
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2023 Visitor KAI:
+      <a class="text-reset text-decoration-none" href="#">PT.Kereta Api Indonesia (Persero) Divre III Palembang</a>
+    </div>
+  </footer>
 </div>
