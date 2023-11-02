3e72ea7777305f356a6f2eed0750dee45041ea4f
yogamaulana11 <yogamaulanaa@gmail.com>
2023-11-02 18:21:17 +0700

    update lupa password

--- resources/views/livewire/admin/lupa-password.blade.php
+++ resources/views/livewire/admin/lupa-password.blade.php
@@ -3,5 +3,39 @@
         <div class="container-fluid">
             <a class="navbar-brand ms-4" href="/">
                 <img src="{{ asset('assets/img/logo.png') }}" alt="KAI"  height="24">
-              </a>
-</div>
+            </a>
+            <div class="me-4">
+                <a href="/login" class="btn text-light rounded-pill fw-semibold" style="background-color: #ED6B23; border-color: #ED6B23">Masuk
+                </a>
+                <a href="/register" class="btn text-light rounded-pill fw-semibold" style="background-color: #2D2A70; border-color: #2D2A70">Registrasi
+                </a>
+                </div>
+        </div>
+          </nav>
+          <div class="container mt-5">
+                <div class="mx-auto" style="width: 500px; height: 500px;">
+                <form>
+                        <div class="mb-3">
+                        <h1 style="text-align:center;">Lupa Kata Sandi</h1>
+                          <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
+                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan Nama Lengkap Anda">
+                        </div>
+                        <div class="mb-3">
+                          <label for="exampleInputPassword1" class="form-label">Email</label>
+                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="admin@admin.com">
+                        </div>
+                        <div class="mb-3">
+                          <label for="exampleInputPassword1" class="form-label">kata Sandi</label>
+                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukan Kata Sandi Anda">
+                        </div>
+                        <div class="mb-3 form-check">
+                        </div>
+                        <button type="submit" class="btn text-light rounded-pill  " style="background-color: #ED6B23">Daftar</button>
+                      </form>
+                      <p class="text-center">
+                      <p class="text-center">Sudah punya akun? <a href="" class="text-center">Masuk</a></p> 
+                            </p>
+                </div>
+            </div>
+            </div>
+    </div>
