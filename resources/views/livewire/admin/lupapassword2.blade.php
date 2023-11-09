d8cdac29942bbcd918af6668d9b02f6114939de5
yogamaulana11 <yogamaulanaa@gmail.com>
2023-11-09 11:29:31 +0700

    menambahkan font poppins

--- resources/views/livewire/admin/lupapassword2.blade.php
+++ resources/views/livewire/admin/lupapassword2.blade.php
@@ -1,10 +1,14 @@
 <head>
   <!-- Sertakan Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
+<!-- Sertakan Font Poppins dari Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
   <!-- Sertakan Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <style>
+    body {
+    font-family: 'Poppins', sans-serif;
+}
     #footer {
     position: fixed;
     bottom: -3%;
