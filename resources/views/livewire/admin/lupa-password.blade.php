81ed9e2c66965f2fdd3e16fedf3d36ec18f26eb0
yogamaulana11 <yogamaulanaa@gmail.com>
2023-11-15 14:08:31 +0700

    update lupa password

--- resources/views/livewire/admin/lupa-password.blade.php
+++ resources/views/livewire/admin/lupa-password.blade.php
@@ -2,16 +2,13 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <a class="navbar-brand ms-4" href="/">
-                <img src="{{ asset('assets/img/logo.png') }}" alt="KAI"  height="24">
+              <img src="{{ asset('assets/img/logo.png') }}" alt="KAI"  height="24">
             </a>
             <div class="me-4">
-                <a href="/login" class="btn text-light rounded-pill fw-semibold" style="background-color: #ED6B23; border-color: #ED6B23">Masuk
-                </a>
-                <a href="/register" class="btn text-light rounded-pill fw-semibold" style="background-color: #2D2A70; border-color: #2D2A70">Registrasi
-                </a>
-                </div>
+              <a href="" class="btn text-dark rounded-pill fw-normal">Masuk</a>
+            </div>
         </div>
-      </nav>
+    </nav>
           <div class="container py-5">
           <div class="container mt-5">
               <div class="mx-auto" style="width: 500px; height: 500px;">
@@ -19,13 +16,15 @@
               <form>
                       <div class="mb-2">
                         <h4 class="text-center mt-3">Lupa Kata Sandi</h4>
-                          <label for="exampleInputPassword1" class="form-label">Email</label>
-                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan email Anda">
+                          <label for="exampleInputPassword1" class="form-label">NIPP</label>
+                          <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Masukkan NIPP Anda">
+                          <label for="exampleInputPassword1" class="form-label">No.Telp</label>
+                          <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Masukkan No.Telp Anda">
                       </div>
                       <div 
                         class="mt-3 form-check">
                       </div>
-                        <button type="submit" class="btn text-light rounded-pill  " style="background-color: #ED6B23">Reset Kata Sandi</button>
+                        <button type="submit" class="btn text-light rounded-pill  " style="background-color: #ED6B23">Konfirmasi</button>
               </form>
               <p class="text-center">
                 <i class="bi bi-arrow-left-square"></i>
@@ -33,10 +32,17 @@
                 </p>
               </div>
           </div>
-  <footer>
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-      © 2023 Visitor KAI:
-      <a class="text-reset text-decoration-none" href="#">PT.Kereta Api Indonesia (Persero) Divre III Palembang</a>
-    </div>
-  </footer>
+          <footer class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid d-flex justify-content-between align-items-center" style="background-color: rgba(0, 0, 0, 0.05);">
+                <div class="text-start">
+                    © 2023 Visitor KAI:
+                    <a class="text-reset text-decoration-none" href="#">PT.Kereta Api Indonesia (Persero) Divre III Palembang</a>
+                </div>
+                <div class="text-end">
+                    <!-- Isi bagian sebelah kanan footer di sini -->
+                </div>
+            </div>
+        </footer>
+        
+</div>
 </div>
