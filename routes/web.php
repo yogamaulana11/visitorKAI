b7880cb6bae760c68da2953680381a83c3f7c3a3
syahrulr27 <201420037@student.binadarma.ac.id>
2023-11-16 10:02:36 +0700

    update branch kode verifikasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-\
+use App\Livewire\Admin\VerifikasiKode;
 use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Auth\Login;
@@ -22,3 +22,5 @@ Route::get('/', LandingPage::class);
 
 Route::get('login', Login::class)->name('login');
 Route::get('register', Register::class)->name('register');
+Route::get('admin/verifikasikode', VerifikasiKode::class)->name('verifikasikode');
+
