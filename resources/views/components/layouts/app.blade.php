7256fe1da4f4d60bb0dc8b9df83288268c1952ca
syahrulr27 <201420037@student.binadarma.ac.id>
2023-11-17 16:20:09 +0700

    update table tamu

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Landing Page' }}</title>
+        <title>{{ $title ?? 'Visitor KAI Divre III' }}</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap">
--- resources/views/livewire/admin/data-tamu.blade.php
+++ resources/views/livewire/admin/data-tamu.blade.php
@@ -1,3 +1,52 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand ms-4" href="/">
+                <img src="{{ asset('assets/img/logo.png') }}" alt="KAI"  height="24">
+              </a>
+              <div class="dropdown">
+                <button class="btn text-bold rounded-5 dropdown-toggle"  data-bs-toggle="dropdown" aria-expanded="false">
+                  Administrator
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Pengaturan</a></li>
+                  <li><a class="dropdown-item" href="#">Keluar</a></li>
+                </ul>
+              </div>
+        </div>
+      </nav>
+
+      <div class="container mt-3">
+        <h4 class="mb-4">Daftar Tamu</h4>            
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th class="text-center">No</th>
+              <th class="text-center">Nama</th>
+              <th class="text-center">Instansi</th>
+              <th class="text-center">Kontak</th>
+              <th class="text-center">Tujuan</th>
+              <th class="text-center">Konfirmasi</th>
+              <th class="text-center">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td class="text-center">1</td>
+                <td>Syahrul Ramadhan</td>
+                <td>Universitas Bina Darma</td>
+                <td>081274663467</td>
+                <td>Ingin Bertemu Pak Tama Unit IT</td>
+                <td>
+                    <i class="btn bi bi-check-lg"></i>
+                    <i class="btn bi bi-x"></i>
+                </td>
+                <td>
+                    <i class="btn bi bi-eye"></i>
+                    <i class="btn bi bi-pencil-square"></i>
+                    <i class="btn bi bi-trash"></i>
+                </td>
+            </tr>
+          </tbody>
+        </table>
 </div>
