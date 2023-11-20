412c40b7a55be6ed9646e7038195c949f891532d
yogamaulana11 <yogamaulanaa@gmail.com>
2023-11-20 16:33:43 +0700

    new branch aturulangpassword

---
+++ app/Livewire/Admin/AturUlangPassword.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class AturUlangPassword extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.atur-ulang-password');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\Auth\Login;
 use App\Livewire\LandingPage;
 use App\Livewire\Auth\Register;
 use App\Livewire\Admin\AdminLogin;
+use App\Livewire\Admin\AturUlangPassword;
 use App\Livewire\Admin\Registrasi;
 use App\Livewire\Admin\LupaPassword;
 use Illuminate\Support\Facades\Route;
@@ -26,6 +27,7 @@ Route::get('register', Register::class)->name('register');
 Route::get('admin/lupapassword',LupaPassword::class )->name('LupaPassword');
 Route::get('admin/registrasi', Registrasi::class)->name('registrasi');
 Route::get('admin/login', AdminLogin::class)->name('adminlogin');
+Route::get('admin/aturulangpassword', AturUlangPassword::class)->name('aturulangpassword');
 
 
 
