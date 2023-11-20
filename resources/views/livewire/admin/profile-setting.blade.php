84b251ad5e37813e46945102f22e859c47ca8353
syahrulr27 <201420037@student.binadarma.ac.id>
2023-11-20 16:40:11 +0700

    update kata sandi profile setting

--- resources/views/livewire/admin/profile-setting.blade.php
+++ resources/views/livewire/admin/profile-setting.blade.php
@@ -1,15 +1,7 @@
-<head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-<style>
-  body {
-    font-family: 'Poppins', sans-serif;
-  }
-</style>
-</head>
-
 <div>
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <a class="navbar-brand ms-4" href="/">
@@ -47,16 +39,8 @@
                 </div>
             </div>
                 <div class="mb-3">
-                  <label class="form-label" style="">NPP</label>
+                  <label class="form-label" style="">NIPP</label>
                   <input type="text" class="form-control">
-                </div>
-                <div class="mb-3">
-                  <label class="form-label">Kata Sandi</label>
-                  <input type="password" class="form-control" >
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Konfirmasi kata sandi</label>
-                    <input type="password" class="form-control" >
                 </div>
                 <div class="mb-3">
                   <label class="form-label">Nomor Telpon</label>
@@ -81,27 +65,32 @@
               </form>
         </div>
       </div>
-    </div>
-    </div>
-  
-    {{-- <div class="container py-2">
+    </div>  
+    {{-- <div class=""> --}}
       <div class="container mt-3">
-        <div class="container mx-auto border border-2 shadow-lg rounded-2" style="width: 750px">
-          <h3 class="container fw-bold mt-3 mb-4">Ubah email
+        <div class="container mx-auto border border-2 shadow-lg rounded-2">
+          <h3 class="container fw-bold mt-3 mb-4">Ubah Kata Sandi
             <div class="border"></div>
           </h3>
           <div class="container mx-auto">
             <form>
               <div class="mb-3">
-                <label class="form-label">Email</label>
-                <input type="text" class="form-control" placeholder="Masukkan email anda">
+                <label class="form-label">Kata Sandi</label>
+                <input type="password" class="form-control" >
               </div>
-              <button type="submit" class="mt-3 mb-3 btn text-white" style="background-color: #ED6B23">Ubah email</button>
+              <div class="mb-3">
+                  <label class="form-label">Konfirmasi kata sandi</label>
+                  <input type="password" class="form-control" >
+              </div>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-4 mt-4">
+                <button class="btn me-md-2 text-white" type="button" style="background-color: #2D2A70">Batal</button>
+                <button class="btn text-white" type="button" style="background-color: #ED6B23">Simpan Perubahan</button>
+              </div>
             </form>
           </div>
         </div>
       </div>
-    </div> --}}
+    {{-- </div> --}}
 
     
 </div>
