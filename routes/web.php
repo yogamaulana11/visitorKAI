e0b6e4cfaa80a2b3f8d0bb5b0e2ee9ee519813f3
yogamaulana11 <yogamaulanaa@gmail.com>
2023-11-21 17:50:12 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,11 @@ use App\Livewire\Admin\VerifikasiKode;
 use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Auth\Login;
-use App\Livewire\LandingPage;
 use App\Livewire\Auth\Register;
 use App\Livewire\Admin\AdminLogin;
 use App\Livewire\Admin\DataTamu;
 use App\Livewire\Admin\Registrasi;
 use App\Livewire\Admin\LupaPassword;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,14 +22,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/',LandingPage::class)->name( LandingPage::class);
+Route::get('/', LandingPage::class)->name(LandingPage::class);
 Route::get('login', Login::class)->name('login');
 Route::get('register', Register::class)->name('register');
-Route::get('admin/lupapassword',LupaPassword::class )->name('LupaPassword');
+Route::get('admin/lupapassword', LupaPassword::class)->name('LupaPassword');
 Route::get('admin/registrasi', Registrasi::class)->name('registrasi');
 Route::get('admin/login', AdminLogin::class)->name('adminlogin');
 Route::get('admin/datatamu', DataTamu::class)->name('datatamu');
 
 
 Route::get('admin/verifikasikode', VerifikasiKode::class)->name('verifikasikode');
-
