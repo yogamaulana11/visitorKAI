8fca23813a50f5f87884d1de7323e93458387e0b
yogamaulana11 <yogamaulanaa@gmail.com>
2023-11-23 17:07:23 +0700

    edit data tamu

--- resources/views/livewire/admin/data-tamu.blade.php
+++ resources/views/livewire/admin/data-tamu.blade.php
@@ -1,11 +1,9 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg bg-light fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand ms-4" href="/">
                 <img src="{{ asset('assets/img/logo.png') }}" alt="KAI" height="24">
             </a>
-            {{-- button login login redirect with icon --}}
 
             <div class="dropdown me-4">
                 <button class="btn  rounded-pill dropdown-toggle text-black" data-bs-toggle="dropdown">
@@ -17,13 +15,13 @@
                 </ul>
             </div>
     </nav>
-    <div class="container-fluid">
+    <div class="container-fluid position-fixed">
         <div class="row flex-nowrap">
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style="background-color: #2D2A70">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                     <a href="/"
                         class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    
+
                     </a>
                     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
                         id="menu">
@@ -51,33 +49,32 @@
             </div>
             <div class="col py-3">
                 <div>
-
-                    <div class="container mt-3">
+                    <div class="container mt-3  table-responsive">
                         <h4 class="mb-4">Daftar Tamu</h4>
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
-                                    <th class="text-center">No</th>
-                                    <th class="text-center">Nama</th>
-                                    <th class="text-center">Instansi</th>
-                                    <th class="text-center">Kontak</th>
-                                    <th class="text-center">Tujuan</th>
-                                    <th class="text-center">Konfirmasi</th>
-                                    <th class="text-center">Action</th>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Kontak</th>
+                                    <th scope="col">Instansi</th>
+                                    <th scope="col">Tujuan</th>
+                                    <th scope="col">Konfirmasi</th>
+                                    <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td class="text-center">1</td>
-                                    <td>Syahrul Ramadhan</td>
-                                    <td>Universitas Bina Darma</td>
-                                    <td>081274663467</td>
-                                    <td>Ingin Bertemu Pak Tama Unit IT</td>
-                                    <td>
+                                    <th scope="row">1</th>
+                                    <td scope="row">Putra Wahyudi Akbar</td>
+                                    <td scope="row">087801350884</td>
+                                    <td scope="row">Universitas Bina Darma</td>
+                                    <td scope="row">Ingin bertemu pak tamawwwwwwwwwwwwwwww</td>
+                                    <td scope="row">
                                         <i class="btn btn-success bi bi-check-lg"></i>
                                         <i class="btn btn-danger bi bi-x"></i>
                                     </td>
-                                    <td>
+                                    <td scope="row">
                                         <div class="dropdown me-4">
                                             <button class="btn  rounded-pill dropdown-toggle text-black"
                                                 data-bs-toggle="dropdown">
@@ -85,22 +82,19 @@
                                             </button>
                                             <ul class=" dropdown-menu">
                                                 <li>
-                                                <i class="btn bi bi-eye"></i>Lihat</li>
+                                                    <i class="btn bi bi-eye"></i>Lihat
+                                                </li>
                                                 <li><i class="btn bi bi-pencil-square"></i>Edit</li>
                                                 <li><i class="btn bi bi-trash"></i>hapus</li>
                                             </ul>
                                         </div>
                                     </td>
-
                                 </tr>
                             </tbody>
                         </table>
-
                     </div>
                 </div>
-                
             </div>
         </div>
-        <td>
-
-        </td>
+    </div>
+</div>
