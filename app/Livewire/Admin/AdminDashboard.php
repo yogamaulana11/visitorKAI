29d368e53e80090faf9f80d7d9da3179649e8427
yogamaulana11 <yogamaulanaa@gmail.com>
2023-12-04 16:38:23 +0700

    dkadija

--- app/Livewire/Admin/AdminDashboard.php
+++ app/Livewire/Admin/AdminDashboard.php
@@ -10,4 +10,17 @@ class AdminDashboard extends Component
     {
         return view('livewire.admin.admin-dashboard');
     }
+
+    public function index()
+    {
+        // Contoh data chart
+        $chartData = [
+            'January' => 20,
+            'February' => 35,
+            'March' => 40,
+            // Tambahkan data bulan lainnya
+        ];
+
+        return view('chart.index', compact('chartData'));
+    }
 }
